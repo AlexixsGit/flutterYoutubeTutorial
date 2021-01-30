208668148a0a141b05f264166a67ec0d7f7dd4a0
Edward Alexis Ortiz <alexixsortizz@gmail.com>
2021-01-30 13:16:40 -0500

    Image viewer implemented in a fourthpage

--- lib/src/screens/fourth_page.dart
+++ lib/src/screens/fourth_page.dart
@@ -3,11 +3,15 @@ import 'package:flutter/material.dart';
 class FourthPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final String url = ModalRoute.of(context).settings.arguments;
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('Hero'),
+        title: Text('Image view'),
       ),
-      body: Center(),
+      body: Center(
+        child: Image.network(url),
+      ),
     );
   }
 }
